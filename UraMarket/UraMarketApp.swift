b6e6bc57d0657ec57fb22bc46d52c7583d2ad24d
Mac mini <tigrastov@yandex.ru>
2025-01-21 20:21:41 +0100

    empty

---
+++ UraMarket/UraMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  UraMarketApp.swift
+//  UraMarket
+//
+//  Created by Mac mini on 21.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct UraMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
